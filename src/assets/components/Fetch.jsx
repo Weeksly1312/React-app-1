c6288a5d0aa77cfc3bbfe9ae2205392916d3d0ca
Weeksly1312 <S.elhallaoui01@gmail.com>
2023-10-11 08:46:07 +0100

    Fetch in react

---
+++ src/assets/components/Fetch.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+
+export default function Fetch() {
+  const [dataApi, setDataApi] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      const data = await res.json();
+
+      setDataApi(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>User List</h1>
+      {dataApi.map((user) => (
+        <p key={user.id}>{user.name}</p>
+      ))}
+    </div>
+  );
+}
